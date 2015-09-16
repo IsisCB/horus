9fb49451e7027c9a83da4d8bb1377e071ed2191f
sylwester ratowt <sylwesterr@gmail.com>
2015-09-16 14:25:29 -0400

    Deleted local files

--- local/auxfile.pl
+++
@@ -1,7 +0,0 @@
-%aux_files = (
-'C:\CB directory\Aux Files\local\journals.tab' => '',
-'C:\CB directory\Aux Files\subs\horuserrors.tab' => '1149616560',
-'C:\CB directory\Aux Files\local\categories.tab' => '',
-'C:\CB directory\Aux Files\local\subject_type_allowed.txt' => '',
-);
-1;
